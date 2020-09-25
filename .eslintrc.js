4f864ba709c19b2c58348ef0175ba1b0aa4a11b6
Samert Eberlin <samer.eberlin@sinch.com>
2020-09-25 16:55:29 +0200

    Fix env/libs for the correct execution of "tsc -noEmit"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
 };
